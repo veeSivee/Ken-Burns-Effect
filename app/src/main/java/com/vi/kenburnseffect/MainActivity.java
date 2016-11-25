855f5e8631155229c4287b1857f3d84078e89a9e
Taufiqotul Faidah <taufiqotul.faidah@icehousecorp.com>
2016-11-25 16:12:03 +0700

    set generator to ken burns

--- app/src/main/java/com/vi/kenburnseffect/MainActivity.java
+++ app/src/main/java/com/vi/kenburnseffect/MainActivity.java
@@ -2,6 +2,11 @@ package com.vi.kenburnseffect;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Interpolator;
+
+import com.flaviofaria.kenburnsview.KenBurnsView;
+import com.flaviofaria.kenburnsview.RandomTransitionGenerator;
+import com.flaviofaria.kenburnsview.Transition;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //init();
+    }
+
+    private void init(){
+        KenBurnsView kenBurnsView = (KenBurnsView)findViewById(R.id.iv_ken_burns);
+        kenBurnsView.setTransitionListener(new KenBurnsView.TransitionListener() {
+            @Override
+            public void onTransitionStart(Transition transition) {
+
+            }
+
+            @Override
+            public void onTransitionEnd(Transition transition) {
+
+            }
+        });
+
+        long duration = 3000;
+        Interpolator interpolator = new Interpolator() {
+            @Override
+            public float getInterpolation(float v) {
+                return 0;
+            }
+        };
+        RandomTransitionGenerator generator = new RandomTransitionGenerator(duration,interpolator);
+        kenBurnsView.setTransitionGenerator(generator);
     }
 }
